8f9b864a377bbc59e6f5b2a969b0b4cc2989181a
yangSir <asdf1995905@gmail.com>
2019-01-17 22:56:18 +0800

    model override toArray demo

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'body'];
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+
+        $arrayAuthor = [
+            'name' => 'Jessie',
+            'link' => 'http://sfsdfsdf.com',
+        ];
+
+        $array['author'] = $arrayAuthor;
+
+        return $array;
+
+    }
 }
